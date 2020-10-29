334ffb62c87c02375af739eab1dfca4db341e36a
Polina Tretyakova <72070079+nepolya@users.noreply.github.com>
2020-10-29 19:55:34 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,12 @@
-from collections import Counter
-a=input()
-c=Counter(a).most_common()
-print(c[0])
+user_input=input()
+most_frequent_character=""
+max_num=0
+for i in user_input:
+    b=0
+    for i1 in user_input:
+        if i1 == i:
+            b+=1
+    if b > max_num:
+        max_num=b
+        most_frequent_character=i
+print(most_frequent_character)
